3e0efcb2b9cd487d8e62e56ae092576e205600a4
Michał <kwestorowski.m@gmail.com>
2017-02-17 20:32:55 +0100

    Added functions for other buttons

--- Koty - Strona glowna/js/index.js
+++ Koty - Strona glowna/js/index.js
@@ -13,3 +13,51 @@ $( ".cross" ).hide();
 $( ".hamburger" ).show();
 });
 });
+
+$( ".cross2" ).hide();
+$( ".menu2" ).hide();
+$( ".hamburger2" ).click(function() {
+    $( ".menu2" ).slideToggle( "slow", function() {
+        $( ".hamburger2" ).hide();
+        $( ".cross2" ).show();
+    });
+});
+
+$( ".cross2" ).click(function() {
+    $( ".menu2" ).slideToggle( "slow", function() {
+        $( ".cross2" ).hide();
+        $( ".hamburger2" ).show();
+    });
+});
+
+$( ".cross3" ).hide();
+$( ".menu3" ).hide();
+$( ".hamburger3" ).click(function() {
+    $( ".menu3" ).slideToggle( "slow", function() {
+        $( ".hamburger3" ).hide();
+        $( ".cross3" ).show();
+    });
+});
+
+$( ".cross3" ).click(function() {
+    $( ".menu3" ).slideToggle( "slow", function() {
+        $( ".cross3" ).hide();
+        $( ".hamburger3" ).show();
+    });
+});
+
+$( ".cross4" ).hide();
+$( ".menu4" ).hide();
+$( ".hamburger4" ).click(function() {
+    $( ".menu4" ).slideToggle( "slow", function() {
+        $( ".hamburger4" ).hide();
+        $( ".cross4" ).show();
+    });
+});
+
+$( ".cross4" ).click(function() {
+    $( ".menu4" ).slideToggle( "slow", function() {
+        $( ".cross4" ).hide();
+        $( ".hamburger4" ).show();
+    });
+});
