7c180c4890ac4c6b1e7bba84a7268d8620c14868
michalkwestorowski <Kwestorowski.m@gmail.com>
2017-02-18 14:01:53 +0100

    Fixed buttons

--- Koty-StronaGlowna/js/index.js
+++ Koty-StronaGlowna/js/index.js
@@ -1,14 +1,14 @@
 $( ".cross" ).hide();
 $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".menu" ).slideToggle( "fast", function() {
         $( ".hamburger" ).hide();
         $( ".cross" ).show();
     });
 });
 
 $( ".cross" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".menu" ).slideToggle( "fast", function() {
         $( ".cross" ).hide();
         $( ".hamburger" ).show();
     });
@@ -17,14 +17,14 @@ $( ".cross" ).click(function() {
 $( ".cross2" ).hide();
 $( ".menu2" ).hide();
 $( ".hamburger2" ).click(function() {
-    $( ".menu2" ).slideToggle( "slow", function() {
+    $( ".menu2" ).slideToggle( "fast", function() {
         $( ".hamburger2" ).hide();
         $( ".cross2" ).show();
     });
 });
 
 $( ".cross2" ).click(function() {
-    $( ".menu2" ).slideToggle( "slow", function() {
+    $( ".menu2" ).slideToggle( "fast", function() {
         $( ".cross2" ).hide();
         $( ".hamburger2" ).show();
     });
@@ -33,14 +33,14 @@ $( ".cross2" ).click(function() {
 $( ".cross3" ).hide();
 $( ".menu3" ).hide();
 $( ".hamburger3" ).click(function() {
-    $( ".menu3" ).slideToggle( "slow", function() {
+    $( ".menu3" ).slideToggle( "fast", function() {
         $( ".hamburger3" ).hide();
         $( ".cross3" ).show();
     });
 });
 
 $( ".cross3" ).click(function() {
-    $( ".menu3" ).slideToggle( "slow", function() {
+    $( ".menu3" ).slideToggle( "fast", function() {
         $( ".cross3" ).hide();
         $( ".hamburger3" ).show();
     });
@@ -49,14 +49,14 @@ $( ".cross3" ).click(function() {
 $( ".cross4" ).hide();
 $( ".menu4" ).hide();
 $( ".hamburger4" ).click(function() {
-    $( ".menu4" ).slideToggle( "slow", function() {
+    $( ".menu4" ).slideToggle( "fast", function() {
         $( ".hamburger4" ).hide();
         $( ".cross4" ).show();
     });
 });
 
 $( ".cross4" ).click(function() {
-    $( ".menu4" ).slideToggle( "slow", function() {
+    $( ".menu4" ).slideToggle( "fast", function() {
         $( ".cross4" ).hide();
         $( ".hamburger4" ).show();
     });
