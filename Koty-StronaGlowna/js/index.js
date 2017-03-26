b4905f8c013809d06af3b8be72466a7696dcab47
michalkwestorowski <Kwestorowski.m@gmail.com>
2017-03-26 12:13:00 +0200

    Added img for table

--- Koty-StronaGlowna/js/index.js
+++ Koty-StronaGlowna/js/index.js
@@ -3,11 +3,15 @@ $("#buttonPremiera").click(function () {
     $("#cosTam").dialog("open");
 });
 $('#cosTam').prepend("<table></table>");
-for (i = 0; i <= 10; i += 1) {
+for (i = 0; i <= 17; i += 1) {
     $('table').append('<tr></tr>')
 }
 ;
-for (j = 0; j < 10; j++) {
+for (j = 0; j < 24; j++) {
     $('tr').append('<td></td>')
 }
 ;
+
+$('td').click(function(){
+    $(this).css('background-color', 'red')
+});
