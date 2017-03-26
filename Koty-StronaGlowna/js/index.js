2444f7b49263d62a4a9db96033ac06844eb2419d
michalkwestorowski <Kwestorowski.m@gmail.com>
2017-03-26 13:23:33 +0200

    Pushed for wojtek

--- Koty-StronaGlowna/js/index.js
+++ Koty-StronaGlowna/js/index.js
@@ -4,7 +4,8 @@ $("#buttonPremiera").click(function () {
 });
 $('#cosTam').prepend("<table></table>");
 for (i = 0; i <= 17; i += 1) {
-    $('table').append('<tr></tr>')
+    $('table').append('<tr></tr>');
+
 }
 ;
 for (j = 0; j < 24; j++) {
@@ -12,6 +13,39 @@ for (j = 0; j < 24; j++) {
 }
 ;
 
-$('td').click(function(){
-    $(this).css('background-color', 'red')
-});
+
+// $('td').click(function(){
+//     $(this).css('background-color', 'red')
+// });
+
+
+$('#tbl td').each(function( getRandomInt, element ){for (i = 0; i < 462; i += getRandomInt(50,100)){
+    $()
+}})
+
+var wyniki = [];
+
+for (i = getRandomInt(1,7); i < 462; i += getRandomInt(50,100)) {
+ wyniki.push(i)
+}
+
+var index = function() {for (i= 0; i < wyniki.length; i++) {
+return
+}};
+
+wyniki.forEach()
+
+
+
+//Wybor losowej liczby
+
+getRandomInt(5, 10);
+
+function getRandomInt(min, max) {
+    // min = Math.ceil(min);
+    // max = Math.floor(max);
+    var randomNumberForCells = Math.floor(Math.random() * (max - min)) + min;
+    return randomNumberForCells;
+};
+//Koniec wyboru losowej liczby
+
