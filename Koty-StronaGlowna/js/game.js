42c2266ccad4e31d58cfc6a2081246f10a55b177
Michał <kwestorowski.m@gmail.com>
2017-04-24 17:48:53 +0200

    Explosion fixed!

--- Koty-StronaGlowna/js/game.js
+++ Koty-StronaGlowna/js/game.js
@@ -5,9 +5,9 @@ $("#buttonPremiera").click(function () {
     $('#gameStatistic').css('display', 'none');
     $("#gameFrame").dialog("open").css('width', '840').css('background-color', '#21324f').css('border', '2px solid black');
     $("div[role='dialog']").css('left', "20%");
-    $('#overlayWybuch').fadeIn(() => {
+    $('#overlayExplosion').fadeIn(() => {
         setTimeout(function () {
-            $('#overlayWybuch').fadeOut()
+            $('#overlayExplosion').fadeOut()
         }, 1200)
     });
 });
