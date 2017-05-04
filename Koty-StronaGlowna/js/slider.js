46abacbe5711fa089cf2c298ec9511ee45c919db
Michał <kwestorowski.m@gmail.com>
2017-05-04 22:02:38 +0200

    Cosmetic changes for game logic

--- Koty-StronaGlowna/js/slider.js
+++ Koty-StronaGlowna/js/slider.js
@@ -1,3 +1,11 @@
 /**
  * Created by MKwestorowski on 04.05.2017.
  */
+function intervalForSlider() {
+
+    let slider = setInterval(function () {
+        $('#imageZajawka').attr('src', 'icons/home.svg');
+    }, 10000)
+}
+
+intervalForSlider();
