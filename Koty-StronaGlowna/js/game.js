c680b74e8d737aacf48ff1dd53c4415235789252
wojtekkas <kas@post.pl>
2017-05-06 14:06:18 +0200

    Pracujemy nad gra

--- Koty-StronaGlowna/js/game.js
+++ Koty-StronaGlowna/js/game.js
@@ -5,11 +5,11 @@ $("#buttonPremiera").click(function () {
     $('#gameStatistic').css('display', 'none');
     $("#gameFrame").dialog("open").css('width', '840').css('background-color', '#21324f').css('border', '2px solid black');
     $("div[role='dialog']").css('left', "20%");
-    $('#overlayExplosion').fadeIn(() => {
-        setTimeout(function () {
-            $('#overlayExplosion').fadeOut()
-        }, 1200)
-    });
+    // $('#overlayExplosion').fadeIn(() => {
+    //     setTimeout(function () {
+    //         $('#overlayExplosion').fadeOut()
+    //     }, 1200)
+    // });
 });
 
 $('#gameStartButton').click(() => {
@@ -53,16 +53,33 @@ function getRandomInt(min, max) {
 createBoard(5);
 
 
+let zakupy = 0;
 function creatingGame(){
-let wyniki = [];
+    let wyniki = [];
 for (i = getRandomInt(1, 4); i < 25; i += getRandomInt(1,8)) {
     wyniki.push(i)
 }
-    for (j = 0; j < wyniki.length; j += 1) {
-        let rand = wyniki[j + 1];
-        $('td').eq(rand).prepend('<img class= "house" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Map-icon-shop.svg/200px-Map-icon-shop.svg.png"/>');
-        $('td').eq(rand).innerText = Odkurzacz.name + " " + Odkurzacz.price;
-}
+    for (i = 0; i < wyniki.length; i += 1) {
+        let rand = wyniki[i + 1];
+        let cellForHome = $('td').eq(rand);
+        let randomcenaOdkurzacz = Odkurzacz.price + getRandomInt(0,100);
+        cellForHome.attr('price', randomcenaOdkurzacz);
+        cellForHome
+            .prepend('<img style="max-width: 20%; height: auto;" class= "house" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Map-icon-shop.svg/200px-Map-icon-shop.svg.png"/>')
+            .prepend('<p style="background-color: white; border: 1px black solid; color: black; font-size: 16px">' + Odkurzacz.name + " " + randomcenaOdkurzacz + "zł" + '</p>');
+
+
+
+    }
+
+    $('td[price]').off('click').click(function() {
+        var randomcenaOdkurzacz = $(this).attr('price');
+        zakupy += parseInt(randomcenaOdkurzacz)
+        $('#zakupyzrobione').html(zakupy);
+        $('img', 'td').remove();
+        $('p', 'td').remove();
+        });
+
 };
 
 
