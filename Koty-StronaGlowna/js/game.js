92839b7dfcde28e74599f91276b96010f1b2e1d5
Wojtek <kas@post.pl>
2017-05-06 14:57:51 +0200

    3 items to buy, timer remove shops when 0, statistics.

--- Koty-StronaGlowna/js/game.js
+++ Koty-StronaGlowna/js/game.js
@@ -1,4 +1,4 @@
-$("#gameFrame").dialog({autoOpen: false, appendTo: "#footer"});
+$("#gameFrame").dialog({autoOpen: false});
 
 
 $("#buttonPremiera").click(function () {
@@ -54,7 +54,10 @@ createBoard(5);
 
 
 let zakupy = 0;
-function creatingGame(){
+let ilosckupionych = 0;
+let iloscporownywarka = 0;
+
+function creatingGame() {
     let wyniki = [];
 for (i = getRandomInt(1, 4); i < 25; i += getRandomInt(1,8)) {
     wyniki.push(i)
@@ -62,40 +65,103 @@ for (i = getRandomInt(1, 4); i < 25; i += getRandomInt(1,8)) {
     for (i = 0; i < wyniki.length; i += 1) {
         let rand = wyniki[i + 1];
         let cellForHome = $('td').eq(rand);
-        let randomcenaOdkurzacz = Odkurzacz.price + getRandomInt(0,100);
+        let randomcenaOdkurzacz = Odkurzacz.price + getRandomInt(0,200);
         cellForHome.attr('price', randomcenaOdkurzacz);
         cellForHome
             .prepend('<img style="max-width: 20%; height: auto;" class= "house" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Map-icon-shop.svg/200px-Map-icon-shop.svg.png"/>')
             .prepend('<p style="background-color: white; border: 1px black solid; color: black; font-size: 16px">' + Odkurzacz.name + " " + randomcenaOdkurzacz + "zł" + '</p>');
 
+    }
+    $('td[price]').off('click').click(function() {
+        var randomcenaOdkurzacz = $(this).attr('price');
+        zakupy += parseInt(randomcenaOdkurzacz);
+        ilosckupionych++;
+        iloscporownywarka += Odkurzacz.price;
+        $('#zakupyzrobione').html(zakupy + " zł");
+        $('#ilosckupionych').html(ilosckupionych + " przedmiot!");
+        $('#ilosczporownywarka').html(iloscporownywarka + " zł");
+        $('img', 'td').remove();
+        $('p', 'td').remove();
+        creatingGame2()
+    })};
+
+function creatingGame2() {
+    let wyniki = [];
+    for (i = getRandomInt(1, 4); i < 25; i += getRandomInt(1,8)) {
+        wyniki.push(i)
+    }
+    for (i = 0; i < wyniki.length; i += 1) {
+        let rand = wyniki[i + 1];
+        let cellForHome = $('td').eq(rand);
+        let randomcenaPralka = Pralka.price + getRandomInt(0,200);
+        cellForHome.attr('price', randomcenaPralka);
+        cellForHome
+            .prepend('<img style="max-width: 20%; height: auto;" class= "house" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Map-icon-shop.svg/200px-Map-icon-shop.svg.png"/>')
+            .prepend('<p style="background-color: white; border: 1px black solid; color: black; font-size: 16px">' + Pralka.name + " " + randomcenaPralka + "zł" + '</p>');
+
 
 
     }
-
     $('td[price]').off('click').click(function() {
-        var randomcenaOdkurzacz = $(this).attr('price');
-        zakupy += parseInt(randomcenaOdkurzacz)
-        $('#zakupyzrobione').html(zakupy);
+        var randomcenaPralka = $(this).attr('price');
+        zakupy += parseInt(randomcenaPralka);
+        ilosckupionych++;
+        iloscporownywarka += Pralka.price;
+        $('#zakupyzrobione').html(zakupy + " zł");
+        $('#ilosckupionych').html(ilosckupionych + " przedmioty!");
+        $('#ilosczporownywarka').html(iloscporownywarka + " zł");
         $('img', 'td').remove();
         $('p', 'td').remove();
-        });
+        creatingGame3();
+    })};
 
+    function creatingGame3() {
+    let wyniki = [];
+    for (i = getRandomInt(1, 4); i < 25; i += getRandomInt(1,8)) {
+        wyniki.push(i)
+    }
+    for (i = 0; i < wyniki.length; i += 1) {
+        let rand = wyniki[i + 1];
+        let cellForHome = $('td').eq(rand);
+        let randomcenaDron = Dron.price + getRandomInt(0,200);
+        cellForHome.attr('price', randomcenaDron);
+        cellForHome
+            .prepend('<img style="max-width: 20%; height: auto;" class= "house" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Map-icon-shop.svg/200px-Map-icon-shop.svg.png"/>')
+            .prepend('<p style="background-color: white; border: 1px black solid; color: black; font-size: 16px">' + Dron.name + " " + randomcenaDron + "zł" + '</p>');
+
+
+
+    }
+    $('td[price]').off('click').click(function() {
+        var randomcenaDron = $(this).attr('price');
+        zakupy += parseInt(randomcenaDron);
+        ilosckupionych++;
+        iloscporownywarka += Dron.price;
+        $('#zakupyzrobione').html(zakupy + " zł");
+        $('#ilosckupionych').html(ilosckupionych + " przedmioty!");
+        $('#ilosczporownywarka').html(iloscporownywarka + " zł");
+        $('img', 'td').remove();
+        $('p', 'td').remove();
+    });
 };
+
 
 
 function timer() {
     let intervalForTimer = setInterval(function () {
         startTime()
     }, 1000);
-    let countSec = 10;
+    let countSec = 5;
     let divTimer = document.getElementById('timer');
     let pTimerEnd = document.getElementById('timerEnd');
 
     function startTime() {
         if (countSec === 0) {
             clearInterval(intervalForTimer);
+            $('img', 'td').remove();
+            $('p', 'td').remove();
             divTimer.style.display = 'none';
-            pTimerEnd.textContent = 'Czas się skończył spróbuj ponownie'
+            pTimerEnd.textContent = 'Czas się skończył!'
 
         } else {
             countSec--;
